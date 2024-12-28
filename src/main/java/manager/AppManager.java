4a9c5c6b487c73d6c5a2ef98503007a2f3844885
leva <shendonlevka@gmail.com>
2024-12-28 22:06:42 +0200

    Login

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -1,4 +1,30 @@
 package manager;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class AppManager {
+    private WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeMethod
+    public void setup(){
+        driver = new ChromeDriver();
+       driver.manage().window().maximize();
+        // Закомментированный метод чтобы менять разрешение и размер окна. тип не максимайз а на свой заданный размер.
+        //  Dimension d = new Dimension(300,1080);// там где 300 по ширине можно увеличить размер
+        //Resize current window to the set dimension
+        //driver.manage().window().setSize(d);
+         }
+         @AfterMethod
+    public void tearDown(){
+           //if (driver!=null)
+          //  driver.quit();
+         }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,21 @@
 package tests;
 
-public class LoginTests {
+import dto.User;
+import manager.AppManager;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends AppManager {
+    @Test
+    public void loginPositiveTest(){
+        User user = User.builder()
+                .email("shendonlevka@gmail.com")
+                .password("Kakashka1!")
+                .build();
+
+        new HomePage(getDriver()).clickBtnLogin();
+        new LoginPage(getDriver()).login(user);
+
+    }
 }
